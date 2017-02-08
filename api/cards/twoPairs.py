5c4158959e562e24ccc0826d1b2b26eb7d466f6a
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-02-07 23:13:58 -0600

    added poker hands and started unit testing

---
+++ api/cards/twoPairs.py
@@ -0,0 +1,13 @@
+#from cards import Hand
+
+
+class TwoPairs(PokerHand):
+
+    def __init__(self):
+        pass
+
+    def IsMatch(self, hand):
+        pass
+
+    def GetStrength(self, hand):
+        pass
