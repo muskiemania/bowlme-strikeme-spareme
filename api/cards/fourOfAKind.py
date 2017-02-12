411d171c3becd4230e075faabd1c8ce704d03d17
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-02-11 22:22:39 -0600

    all poker hands and unit tests

---
+++ api/cards/fourOfAKind.py
@@ -0,0 +1,17 @@
+from cards import PokerHand
+
+class FourOfAKind(PokerHand):
+
+    def __init__(self):
+        self.__rating = 7
+        PokerHand.__init__(self)
+
+    def IsMatch(self, hand):
+        tally = PokerHand.cardTally(hand)
+        return any(filter(lambda x: len(x) == 4, tally.values()))
+        
+    def GetRating(self, hand):
+        tally = PokerHand.cardTally(hand)
+        quad = {k: v for (k,v) in tally.items() if len(v) == 4}.keys()
+        others = sorted({k:v for (k,v) in tally.items() if len(v) < 4}.keys(), reverse=True)
+        return (self.__rating, quad[0], PokerHand.Coalesce(others, 0, 0), 99, 99, 99)
