a8297ed47f8cc8b1a8a4732cb217b81460957d04
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-06-19 12:46:12 -0500

    lint

--- api/cards/flush.py
+++ api/cards/flush.py
@@ -5,9 +5,9 @@ class Flush(PokerHand):
     def __init__(self):
         self.__rating = 6
 
-    def IsMatch(self, hand):
-        return len(PokerHand.getSuitTally(hand).keys()) == 1 and len(hand.cards) == 5
+    def is_match(self, hand):
+        return len(PokerHand.get_suit_tally(hand).keys()) == 1 and len(hand.cards) == 5
 
-    def GetRating(self, hand):
-        PokerHand.sortCards(hand)
+    def get_rating(self, hand):
+        PokerHand.sort_cards(hand)
         return (self.__rating, hand.cards[0].strength, hand.cards[1].strength, hand.cards[2].strength, hand.cards[3].strength, hand.cards[4].strength)
