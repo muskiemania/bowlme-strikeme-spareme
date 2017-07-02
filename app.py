37b9a170e2b5c5fcb548108ebbc97055b0bb781e
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-07-02 15:49:48 -0500

    initial placeholders for react

---
+++ app.py
@@ -0,0 +1,40 @@
+import cherrypy
+import api.controllers
+
+dispatcher = cherrypy.dispatch.RoutesDispatcher()
+
+dispatcher.connect(name='api_create_game', route='/game/create', controller=api.controllers.CreateGameController(), action='index', conditions=dict(method=['GET']))
+dispatcher.mapper.connect('/game/create', controller='api_create_game', action='create', conditions=dict(method=['POST']))
+
+dispatcher.connect(name='api_get_game', route='/game/{game_id}/player/{player_id}', controller=api.controllers.GameController(), action='index', conditions=dict(method=['GET']))
+
+dispatcher.connect(name='api_join_game', route='/game/join', controller=api.controllers.JoinGameController(), action='index', conditions=dict(method=['GET']))
+dispatcher.mapper.connect('/game/join', controller='api_join_game', action='join', conditions=dict(method=['POST']))
+
+dispatcher.connect(name='api_start_game', route='/game/start', controller=api.controllers.StartGameController(), action='index', conditions=dict(method=['GET']))
+dispatcher.mapper.connect('/game/start', controller='api_start_game', action='start', conditions=dict(method=['POST']))
+
+dispatcher.connect(name='api_draw_cards', route='/game/draw', controller=api.controllers.DrawCardsController(), action='index', conditions=dict(method=['GET']))
+dispatcher.mapper.connect('/game/draw', controller='api_draw_cards', action='draw', conditions=dict(method=['POST']))
+
+dispatcher.connect(name='api_discard_cards', route='/game/discard', controller=api.controllers.DiscardCardsController(), action='discard', conditions=dict(method=['GET']))
+dispatcher.mapper.connect('/game/discard', controller='api_discard_cards', action='discard', conditions=dict(method=['POST']))
+
+#dispatcher.connect(name='api_end_hand', route='/game/hand/end', controller=controllers.EndHandController(), action='index', conditions=dict(method=['GET']))
+#dispatcher.mapper.connect('/game/hand/end', controller='api_end_hand', action='end', conditions=dict(method=['POST']))
+
+#dispatcher.connect(name='api_end_game', route='/game/end', controller=controllers.EndGameController(), action='index', conditions=dict(method=['GET']))
+#dispatcher.mapper.connect('/game/end', controller='api_end_game', action='end', conditions=dict(method=['POST']))
+
+
+conf = {}
+conf['/'] = {'request.dispatch': dispatcher}
+#conf['/static'] = {'tools.staticdir.on': True, 'tools.staticdir.dir': "public"}
+cherrypy.tree.mount(root=None, config=conf)
+
+if hasattr(cherrypy.engine, 'block'):
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+else:
+    cherrypy.server.quickstart()
+    cherrypy.engine.start()
