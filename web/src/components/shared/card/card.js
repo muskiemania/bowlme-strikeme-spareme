7e217bcdafc92d81d9c7eb33f0dd4b12eeb18426
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-09-09 22:06:14 -0500

    cards now have a selected state and you can click on them to highlight

--- web/src/components/shared/card/card.js
+++ web/src/components/shared/card/card.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {render} from 'react-dom';
 import './card.less';
 import Cardinality from '../cardinality/cardinality';
 import Suit from '../suit/suit';
 import classnames from 'classnames';
+import Immutable from 'immutable';
 
 import _ from 'lodash';
 
-class Card extends React.Component {
+class Card extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { isSelected: false };
+
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+    }
+    
     getSuit(card) {
 
         let suit = card.charAt(1);
@@ -31,12 +39,20 @@ class Card extends React.Component {
 
         return card.charAt(0);
     }
+
+    isSelected() {
+        return this.state.isSelected;
+    }
+    
+    handleTouchStart(){
+        this.setState({ isSelected: !this.isSelected() });
+    }
     
     render() {
         let {card, isLast} = this.props;
 
         return (
-            <div className={classnames('column', 'a-card', this.getSuit(card), isLast ? 'last-card' : '')}>
+            <div className={classnames('column', 'a-card', this.getSuit(card), isLast ? 'last-card' : '', this.isSelected() ? 'selected' : '')} onTouchStart={this.handleTouchStart} onClick={this.handleTouchStart}>
                 <Cardinality value={this.getCardinality(card)} />
                 <Suit value={this.getSuit(card)} />
                 <div className={isLast ? 'big' : 'reg'}>
