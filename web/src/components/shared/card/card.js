3e5cee9247795a8556a3a930145467f638e52b46
Justin Muskivitch <muskiemania@MacBook-Pro.local>
2017-09-15 23:29:05 -0500

    switched character translation logic to functions, cleaned up all the dependencies

--- web/src/components/shared/card/card.js
+++ web/src/components/shared/card/card.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {render} from 'react-dom';
 import './card.less';
-import Cardinality from '../cardinality/cardinality';
-import Suit from '../suit/suit';
+import { getCardinality } from '../../../helpers/getCardinality';
+import { getSuit } from '../../../helpers/getSuit';
+import { getSuitName } from '../../../helpers/getSuitName';
 import classnames from 'classnames';
-import Immutable from 'immutable';
 
-import _ from 'lodash';
 
 class Card extends Component {
-
-    getSuit(card) {
-
-        let suit = card.charAt(1);
-
-        let suits = {};
-        suits['C'] = 'clubs';
-        suits['D'] = 'diamonds';
-        suits['H'] = 'hearts';
-        suits['S'] = 'spades';
-        
-        if(suit in suits){
-            return suits[suit];
-        }
-
-        return '';
-    }
-
-    getCardinality(card) {
-
-        return card.charAt(0);
-    }
 
     handleTouchStart(){
         this.props.toggleSelected(this.props.card);
@@ -39,14 +15,26 @@ class Card extends Component {
     
     render() {
         let {card, index, isSelected} = this.props;
-
+        
         return (
-            <div className={classnames('column', 'a-card', this.getSuit(card), `card-${index}`, isSelected ? 'selected' : '')} onTouchStart={this.handleTouchStart.bind(this)} onClick={this.handleTouchStart.bind(this)}>
-                <Cardinality value={this.getCardinality(card)} />
-                <Suit value={this.getSuit(card)} />
-                <div className={'big'}>
-                    <Suit value={this.getSuit(card)} />
-                </div>         
+            <div className={classnames('column', 'a-card', getSuitName(card), `card-${index}`, isSelected ? 'selected' : '')} onTouchStart={this.handleTouchStart.bind(this)} onClick={this.handleTouchStart.bind(this)}>
+                <div className='cardinality'>
+                    <span>
+                        { getCardinality(card) }
+                    </span>
+                </div>
+                <div className='suit'>
+                    <span>
+                        { getSuit(card) }
+                    </span>
+                </div>
+                <div className='big'>
+                    <div className='suit'>
+                        <span>
+                            { getSuit(card) }
+                        </span>
+                    </div>
+                </div>
             </div>
         );
     }
