13ce3319648b0aeb039bcbde6120e34ce6fb14de
Justin <muskiemania@gmail.com>
2018-08-21 08:00:04 -0500

    created separate verify, updated pylint config

---
+++ api/lambdas/verify.py
@@ -0,0 +1,16 @@
+from lambdas import game
+from lambdas.bowl_redis_dto import GameStatus
+
+def lambda_handler(event, context):
+    game_id = None #cherrypy.request.cookie['game_auth']
+    player_id = None #cherrypy.request.cookie['player_auth']
+
+    game_verified = game.Verify.verify_game_by_id(game_id, [GameStatus.CREATED])
+    player_verified = game.Verify.verify_player_in_game(game_id, player_id)
+    player_is_host = game.Verify.verify_player_is_host(game_id, player_id)
+
+    return {
+        'gameVerified': game_verified,
+        'playerVerified': player_verified,
+        'playerIsHost': player_is_host
+    }
