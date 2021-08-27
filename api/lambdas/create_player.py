967fe1c5af0d762839643b842dd517a5943c31dd
muskiemania <muskiemania@gmail.com>
2021-08-26 20:21:38 -0500

    updated this

--- api/lambdas/create_player.py
+++ api/lambdas/create_player.py
@@ -5,18 +5,18 @@ import bowl_game
 def handler(event, context):
 
     #fetch input...
-    player_name = event['playerName']
-    game_key = event['key']
-    game_id = event['gameId']
-
-    #create player
-    created_player = bowl_game.CreatePlayer.create(player_name, game_key)
-
-    #create jwt
-    #if not created_game.is_game_id_zero():
-
-    #    jwt = Helpers().get_jwt(created_game.get_jwt_data())
-    #    created_game.set_jwt(jwt)
-
-    created_player['gameId'] = game_id
-    return created_player
+    player_name = event.get('playerName', 'Anonymous')
+    game_hash = event.get('gameId')
+    
+    if game_hash is None:
+        raise Exception('gameId is required')
+    
+    player_id = dynamos.CreatePlayer.create(player_name, game_hash, False)
+    
+    return {
+        'statusCode': 200,
+        'body': {
+            'gameId': game_hash,
+            'playerId': player_id
+        }
+    }
