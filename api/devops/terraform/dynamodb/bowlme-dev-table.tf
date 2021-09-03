da0bb88f671631701ad425015dae95e3ae3752ea
EC2 Default User <ec2-user@ip-172-26-4-75.us-east-2.compute.internal>
2021-09-03 01:20:46 +0000

    added dev table to terraform

---
+++ api/devops/terraform/dynamodb/bowlme-dev-table.tf
@@ -0,0 +1,9 @@
+resource "aws_dynamodb_table" "bowlme-dev" {
+	name         = "bowlme-dev"
+	billing_mode = "PAY_PER_REQUEST"
+  	hash_key     = "game_id"
+  	attribute {
+    		name = "game_id"
+    		type = "S"
+  	}
+}
