bc4b8fa32be421acb3e8383d1c5e387723213cd0
EC2 Default User <ec2-user@ip-172-26-4-75.us-east-2.compute.internal>
2021-09-04 03:51:27 +0000

    fixing api gateway permissions

--- api/devops/terraform/api_gateway/create_game_POST.tf
+++ api/devops/terraform/api_gateway/create_game_POST.tf
@@ -4,7 +4,13 @@ resource "aws_apigatewayv2_integration" "bowlme-v2-lambda-create-game-integratio
 
     	integration_method   = "POST"
     	integration_uri      = var.bowlme_v2_create_game_lambda_arn
-    	passthrough_behavior = "WHEN_NO_MATCH"
+
+	response_parameters {
+        	mappings    = {
+              		"overwrite:header.Content-Type" = "application/json"
+            	}
+          	status_code = "200"
+        }
 }
 
 resource "aws_apigatewayv2_route" "lambda-route" {
@@ -13,10 +19,11 @@ resource "aws_apigatewayv2_route" "lambda-route" {
 	target             = "integrations/${aws_apigatewayv2_integration.bowlme-v2-lambda-create-game-integration.id}"
 }
 
-resource "aws_lambda_permission" "api-gw" {
+resource "aws_lambda_permission" "allow_api_gateway" {
 	statement_id  = "AllowExecutionFromAPIGateway"
 	action        = "lambda:InvokeFunction"
-	function_name = var.bowlme_v2_create_game_lambda_arn
-	principal     = "apigateway.amazonaws.com"
-	source_arn    = "${aws_apigatewayv2_api.bowlme-v2-lambda-api.execution_arn}/*/*/*"
+      	function_name = var.bowlme_v2_create_game_lambda_arn
+      	principal     = "apigateway.amazonaws.com"
+      	source_arn    = "arn:aws:execute-api:us-west-2:359299993558:6ra4b3z96j/*/*/game/create"
 }
+
--- api/devops/terraform/api_gateway/main.tf
+++ api/devops/terraform/api_gateway/main.tf
@@ -1,12 +1,30 @@
 resource "aws_apigatewayv2_api" "bowlme-v2-lambda-api" {
-	name          = "bowlme-v2-lambda-api"
-	protocol_type = "HTTP"
+	name                 = "bowlme-v2-lambda-api"
+	protocol_type        = "HTTP"
 }
 
 resource "aws_apigatewayv2_stage" "bowlme-v2-lambda-stage" {
 	api_id      = aws_apigatewayv2_api.bowlme-v2-lambda-api.id
     	name        = "$default"
-   	auto_deploy = true
+   	access_log_settings {
+		destination_arn = "${aws_cloudwatch_log_group.bowlme-v2-api-log-group.arn}"
+		format          = jsonencode({
+        		httpMethod     = "$context.httpMethod"
+        		ip             = "$context.identity.sourceIp"
+        		protocol       = "$context.protocol"
+        		requestId      = "$context.requestId"
+        		requestTime    = "$context.requestTime"
+        		responseLength = "$context.responseLength"
+        		routeKey       = "$context.routeKey"
+        		status         = "$context.status"
+			errorMessage   = "$context.error.messageString"
+      		})
+	}
+	auto_deploy = true
 }
 
+resource "aws_cloudwatch_log_group" "bowlme-v2-api-log-group" {
+  	name              = "API-Gateway-Execution-Logs_${aws_apigatewayv2_api.bowlme-v2-lambda-api.id}/default"
+  	retention_in_days = 7
+}
 
