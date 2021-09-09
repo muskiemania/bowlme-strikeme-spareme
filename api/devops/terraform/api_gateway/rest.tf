33d8d08a0536bf9f76998f9848142f5df928b945
EC2 Default User <ec2-user@ip-172-26-4-75.us-east-2.compute.internal>
2021-09-09 03:01:20 +0000

    sfn integration is ok, but input from api to sfn needs transform

---
+++ api/devops/terraform/api_gateway/rest.tf
@@ -0,0 +1,94 @@
+resource "aws_api_gateway_rest_api" "bowlme-create-game" {
+	name = "bowlme-create-game"
+}
+
+resource "aws_api_gateway_resource" "game" {
+  	parent_id   = aws_api_gateway_rest_api.bowlme-create-game.root_resource_id
+  	path_part   = "game"
+  	rest_api_id = aws_api_gateway_rest_api.bowlme-create-game.id
+}
+
+resource "aws_api_gateway_resource" "create" {
+  	parent_id   = aws_api_gateway_resource.game.id
+  	path_part   = "create"
+  	rest_api_id = aws_api_gateway_rest_api.bowlme-create-game.id
+}
+
+resource "aws_api_gateway_method" "create" {
+  	authorization = "NONE"
+  	http_method   = "POST"
+  	resource_id   = aws_api_gateway_resource.create.id
+  	rest_api_id   = aws_api_gateway_rest_api.bowlme-create-game.id
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+	rest_api_id = aws_api_gateway_rest_api.bowlme-create-game.id
+  	resource_id = aws_api_gateway_resource.create.id
+  	http_method = aws_api_gateway_method.create.http_method
+  	status_code = "200"
+}
+
+#resource "aws_api_gateway_integration_response" "response_200" {
+#	rest_api_id = aws_api_gateway_rest_api.bowlme-create-game.id
+#	resource_id = aws_api_gateway_resource.create.id
+#	http_method = aws_api_gateway_method.create.http_method
+#	status_code = aws_api_gateway_method_response.response_200.status_code
+#
+#	response_templates = {
+#		"application/json" = ""
+#	}
+#}
+
+resource "aws_api_gateway_integration" "proxy-to-sfn" {
+  	http_method             = aws_api_gateway_method.create.http_method
+  	integration_http_method = aws_api_gateway_method.create.http_method
+	resource_id             = aws_api_gateway_resource.create.id
+  	rest_api_id             = aws_api_gateway_rest_api.bowlme-create-game.id
+  	type                    = "AWS"
+	passthrough_behavior    = "WHEN_NO_MATCH"
+	credentials             = "arn:aws:iam::359299993558:role/bowlme_sfn_role"
+	uri                     = "arn:aws:apigateway:us-west-2:states:action/StartSyncExecution"
+
+	request_templates       = {
+		"application/json" = <<EOF
+			#set($data = $util.escapeJavaScript($input.json('$')))
+			{
+				"input": {
+					"body": "$data"
+				},
+				"stateMachineArn": "arn:aws:states:us-west-2:359299993558:stateMachine:bowlme-v2-us-west-2-create-game"
+			}
+			EOF
+	}
+}
+
+resource "aws_api_gateway_deployment" "bowlme-create" {
+  	rest_api_id = aws_api_gateway_rest_api.bowlme-create-game.id
+
+  	triggers = {
+    	# NOTE: The configuration below will satisfy ordering considerations,
+    	#       but not pick up all future REST API changes. More advanced patterns
+    	#       are possible, such as using the filesha1() function against the
+    	#       Terraform configuration file(s) or removing the .id references to
+    	#       calculate a hash against whole resources. Be aware that using whole
+    	#       resources will show a difference after the initial implementation.
+    	#       It will stabilize to only change when resources change afterwards.
+    		redeployment = sha1(jsonencode([
+      			aws_api_gateway_resource.create.id,
+      			aws_api_gateway_method.create.id,
+      			aws_api_gateway_integration.proxy-to-sfn.id,
+			aws_api_gateway_method_response.response_200,
+    			#aws_api_gateway_integration_response.response_200
+		]))
+  	}
+
+  	lifecycle {
+    		create_before_destroy = true
+  	}
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  	deployment_id = aws_api_gateway_deployment.bowlme-create.id
+  	rest_api_id   = aws_api_gateway_rest_api.bowlme-create-game.id
+  	stage_name    = "dev"
+}
