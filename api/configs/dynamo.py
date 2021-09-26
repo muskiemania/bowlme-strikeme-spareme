0ae714bc1ea60ddaa19492d8820e7397e46093ef
EC2 Default User <ec2-user@ip-172-26-4-75.us-east-2.compute.internal>
2021-09-26 03:38:06 +0000

    fixed name mismatch

--- api/configs/dynamo.py
+++ api/configs/dynamo.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class DynamoConfigs(Enum):
     GAME_TABLE_NAME = f'bowlme-game-{os.environ["environ"]}'
-    PLAYERS_TABLE_NAME = f'bowlme-players-{os.environ["environ"]}'
+    PLAYER_TABLE_NAME = f'bowlme-player-{os.environ["environ"]}'
     LEADERBOARD_TABLE_NAME = f'bowlme-leaderboard-{os.environ["environ"]}'
     DECK = 'deck',
     DISCARD = 'discard'
